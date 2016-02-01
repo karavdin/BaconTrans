f23d6c512654dc149ec7ad446ce3fa5fa5831fb6
Anastasia Karavdina <karavdina@gmail.com>
2016-02-01 14:04:21 +0100

    change ways to files

--- load.C
+++ load.C
@@ -1,6 +1,6 @@
 {
-  gSystem->AddIncludePath("-I/afs/desy.de/user/k/karavdia/CMSSW_7_4_9/UHH2/BaconTrans/baconheaders/");
-  gSystem->AddLinkedLibs("-L/afs/desy.de/user/k/karavdia/SFrame_04_00_01_root602/lib -lSUHH2bacondataformats");
+  gSystem->AddIncludePath("-I/afs/desy.de/user/k/karavdia/CMSSW_7_6_3_patch1/src/UHH2/BaconTrans/baconheaders/");
+  gSystem->AddLinkedLibs("-L/afs/desy.de/user/k/karavdia/SFrame_04_00_01_CMSSW76X/lib -lSUHH2bacondataformats");
   gSystem->SetFlagsOpt("-O2 -DNDEBUG -Wno-missing-field-initializers");
   // gROOT->LoadMacro("baconheaders/TEventInfo.hh+");
   // gROOT->LoadMacro("baconheaders/TGenEventInfo.hh+");
@@ -10,5 +10,6 @@
   // gROOT->LoadMacro("baconheaders/TVertex.hh+");
 
   
-  gROOT->LoadMacro("UHH2/BaconTrans/convert.C+");
+  //  gROOT->LoadMacro("UHH2/BaconTrans/convert.C+");
+  gROOT->LoadMacro("convert.C+");
 }
--- test.C
+++ test.C
@@ -3,11 +3,13 @@
   //convert("/nfs/dust/cms/user/kovalch/CRAB/ZeroBias/jme_1.root","test.root","nt_AK4PFCluster");
   //convert("/pnfs/desy.de/cms/tier2/store/user/stadie/ZeroBias1/zero/150625_132419/0000/jme_1.root","test.root","nt_AK4PFCluster");
   std::string first,second,path;
-  first = "/pnfs/desy.de/cms/tier2/store/user/nkovalch/ZeroBias4/zero/150702_093020/0000/jme_";
+  //  first = "/pnfs/desy.de/cms/tier2/store/user/nkovalch/ZeroBias4/zero/150702_093020/0000/jme_";
+  first = "/nfs/dust/cms/user/karavdia/GridControl_JEC/JMEValidatorTree_13TeV_DATA_RunIID_Cert_246908-260627_v2_13TeV_PromptReco_Collisions15_25ns_JSON_Summer15_25nsV6_DATA/dataJetHTRun2015D16Dec_";
   second = ".root";
   std::string tmp;
   Long_t *id,*size,*flags,*mt;
-  for (int i=1; i<115; i++){
+  for (int i=1; i<1424; i++){
+  //  for (int i=0; i<1; i++){
     tmp = std::to_string(i);
     path = first+tmp+second;
     const char *tmp_ = path.c_str();
@@ -16,7 +18,7 @@
     // conversion if the file exists...
     if(!gSystem->GetPathInfo(tmp_,id,size,flags,mt)){ 
       std::cout << "    ++++++    conversion of " << path << " to bacon data format    ++++++" << std::endl;
-      convert(first+tmp+second,"/nfs/dust/cms/user/mstoev/CMSSW_7_2_1_patch4/src/UHH2/BaconTrans/zeroT_ak4pfcluster/jme_ak4pfcluster_ZeroBias4_"+tmp+".root","nt_AK4PFCluster");
+      convert(first+tmp+second,"/nfs/dust/cms/user/karavdia/BaconTrans_JEC/AK4PFCHS/JMEValidatorTree_13TeV_DATA_RunIID_Cert_246908-260627_v2_13TeV_PromptReco_Collisions15_25ns_JSON_Summer15_25nsV6_DATA/dataJetHTRun2015D16Dec_"+tmp+".root","AK4PFCHS");
     }
   }
 }
