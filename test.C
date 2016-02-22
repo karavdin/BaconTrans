58bed13d05bb0b331cf4de90e9e6147ee0443e91
Anastasia Karavdina <karavdina@gmail.com>
2016-02-22 16:27:13 +0100

    up to date file

--- test.C
+++ test.C
@@ -4,12 +4,14 @@
   //convert("/pnfs/desy.de/cms/tier2/store/user/stadie/ZeroBias1/zero/150625_132419/0000/jme_1.root","test.root","nt_AK4PFCluster");
   std::string first,second,path;
   //  first = "/pnfs/desy.de/cms/tier2/store/user/nkovalch/ZeroBias4/zero/150702_093020/0000/jme_";
-  first = "/nfs/dust/cms/user/karavdia/GridControl_JEC/JMEValidatorTree_13TeV_DATA_RunIID_Cert_246908-260627_v2_13TeV_PromptReco_Collisions15_25ns_JSON_Summer15_25nsV6_DATA/dataJetHTRun2015D16Dec_";
+  //  first = "/nfs/dust/cms/user/karavdia/GridControl_JEC/JMEValidatorTree_QCD_Pt-15to7000_TuneCUETHS1_Flat_13TeV_herwigpp_RunIIFall15MiniAODv1_Fall15_25nsV1_MC/mcflatherwigpp_";
+  first = "/nfs/dust/cms/user/karavdia/GridControl_JEC/JMEValidatorTree_QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8_RunIIFall15MiniAODv1_Fall15_25nsV1_MC/mcflatpythia8_";
   second = ".root";
   std::string tmp;
   Long_t *id,*size,*flags,*mt;
   //  for (int i=0; i<1424; i++){
-  for (int i=0; i<10; i++){
+  for (int i=0; i<262; i++){
+  //  for (int i=0; i<226; i++){
     tmp = std::to_string(i);
     path = first+tmp+second;
     const char *tmp_ = path.c_str();
@@ -18,7 +20,8 @@
     // conversion if the file exists...
     if(!gSystem->GetPathInfo(tmp_,id,size,flags,mt)){ 
       std::cout << "    ++++++    conversion of " << path << " to bacon data format    ++++++" << std::endl;
-      convert(first+tmp+second,"/nfs/dust/cms/user/karavdia/BaconTrans_JEC2/AK4PFCHS/JMEValidatorTree_13TeV_DATA_RunIID_Cert_246908-260627_v2_13TeV_PromptReco_Collisions15_25ns_JSON_Summer15_25nsV6_DATA/dataJetHTRun2015D16Dec_"+tmp+".root","AK4PFCHS");
+      //    convert(first+tmp+second,"/nfs/dust/cms/user/karavdia/BaconTrans_JEC/AK4PFCHS/JMEValidatorTree_QCD_Pt-15to7000_TuneCUETHS1_Flat_13TeV_herwigpp_RunIIFall15MiniAODv1_Fall15_25nsV1_MC/mcflatherwigpp_"+tmp+".root","AK4PFCHS");
+      convert(first+tmp+second,"/nfs/dust/cms/user/karavdia/BaconTrans_JEC/AK8PFCHS/JMEValidatorTree_QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8_RunIIFall15MiniAODv1_Fall15_25nsV1_MC/mcflatpythia8_"+tmp+".root","AK8PFCHS");
     }
   }
 }
